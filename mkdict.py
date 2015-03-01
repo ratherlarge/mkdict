34e2970fbb28ba8b73a52586f31e895cb3d60050
ratherlarge <ratherlargerer@gmail.com>
2015-03-01 11:15:34 +0000

    Update mkdict.py

--- mkdict.py
+++ mkdict.py
@@ -6,28 +6,28 @@ class mkdict(object):
             self.fullkey = fullkey
             
     def __init__(self, d={}, **kwargs):
-        self._dict = dict()
+        self.dict = dict()
         self._keymap = dict()
         self._dict_backup = None
         self._keymap_backup
         self.update(d, **kwargs)
         
     def __str__(self):
-        return str(self._dict)
+        return str(self.dict)
         
     def __len__(self):
-        return len(self._dict)
+        return len(self.dict)
         
     def __getitem__(self, key):
         if key in self._keymap:
             key = self.fullkey(key)
-        return self._dict[key]
+        return self.dict[key]
         
     def __setitem__(self, key, value):
         if key in self._keymap:
             key = self.fullkey(key)
             
-        if key not in self._dict:
+        if key not in self.dict:
             if isinstance(key, tuple):
                 key = tuple(set(key))
                 fullkey_ptr = self._FullKeyPtr(key)
@@ -38,7 +38,7 @@ class mkdict(object):
             else:
                 self._keymap[key] = self._FullKeyPtr(key)
                 
-        self._dict[key] = value
+        self.dict[key] = value
         
     def __delitem__(self, key):
         fullkey = self.fullkey(key)
@@ -49,35 +49,25 @@ class mkdict(object):
         else:
             del self._keymap[fullkey]
             
-        del self._dict[fullkey]
+        del self.dict[fullkey]
         
     def __contains__(self, key):
-        return key in self._keymap or key in self._dict
+        return key in self._keymap or key in self.dict
         
     def __iter__(self):
-        return iter(self._dict)
+        return iter(self.dict)
+        
+    def __getattr__(self, attr):
+        return getattr(self.dict, attr)
         
     def update(self, d={}, **kwargs):
         d.update(kwargs)
         for k, v in d.items():
             self[k] = v
-            
-    def items(self):
-        return self._dict.items()
-        
-    def keys(self):
-        return self._dict.keys()
-        
-    def pop(self):
-        pass
     
     def clear(self):
-        self._dict.clear()
+        self.dict.clear()
         self._keymap.clear()
-        
-    @property
-    def dict(self):
-        return self._dict.copy()
         
     def fullkey(self, key):
         return self._keymap[key].fullkey
@@ -86,7 +76,7 @@ class mkdict(object):
         pass
     
     def remove(self, key, return_value=False):
-        if key in self._dict:
+        if key in self.dict:
             del self[key]
             return
         
@@ -99,8 +89,8 @@ class mkdict(object):
         else:
             new_fullkey = tuple(new_fullkey)
             
-        self._dict[new_fullkey] = self._dict[current_fullkey]
-        del self._dict[current_fullkey]
+        self.dict[new_fullkey] = self.dict[current_fullkey]
+        del self.dict[current_fullkey]
         self._keymap[key].fullkey = new_fullkey
         del self._keymap[key]
         
@@ -113,12 +103,12 @@ class mkdict(object):
         return list()
         
     def backup(self):
-        self._dict_backup = self.dict
+        self._dict_backup = self.dict.copy()
         self._keymap_backup = self._keymap.copy()
         
     def revert(self):
         if None not in (self._dict_backup, self._keymap_backup):
-            self._dict = self._dict_backup
+            self.dict = self._dict_backup
             self._keymap = self._keymap_backup
             
     def _key_already_set(self, key):
